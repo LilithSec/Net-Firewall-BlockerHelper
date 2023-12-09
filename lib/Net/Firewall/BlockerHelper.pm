d6bce69d86f7f0a543105aa757399e4daecc13ff
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2023-12-09 06:58:22 -0600

    create initial files

---
+++ lib/Net/Firewall/BlockerHelper.pm
@@ -0,0 +1,105 @@
+package Net::Firewall::BlockerHelper;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Net::Firewall::BlockerHelper - The great new Net::Firewall::BlockerHelper!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Net::Firewall::BlockerHelper;
+
+    my $foo = Net::Firewall::BlockerHelper->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.ent> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-net-firewall-blockerhelper at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net-Firewall-BlockerHelper>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Net::Firewall::BlockerHelper
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Net-Firewall-BlockerHelper>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Net-Firewall-BlockerHelper>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Net-Firewall-BlockerHelper>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2023 by Zane C. Bowers-Hadley.
+
+This is free software, licensed under:
+
+  The GNU Lesser General Public License, Version 2.1, February 1999
+
+
+=cut
+
+1; # End of Net::Firewall::BlockerHelper
